6b3683bbc53abb743b14f5c93fa2ed7ecfb889d8
djc666888 <112179089+djc666888@users.noreply.github.com>
2023-10-20 18:33:40 +0800

    Update spotify-json2.js

--- js/spotify-json2.js
+++ js/spotify-json2.js
@@ -1,7 +1,55 @@
 console.log(`spotify-json-2023.06.13`);
+let url = $rconsole.log(`spotify-json-2023.06.13`);
 let url = $request.url;
 // console.log(`原始url:${url}`);
 if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+    // console.log(`替换platform:${url}`);
+} else {
+    console.log('无需处理');
+}
+$done({
+    url
+});
+eqconsole.log(`spotify-json-2023.06.13`);
+let url = $request.url;
+// console.log(`原始url:${url}`);
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+    // console.log(`替换platform:${url}`);
+} else {
+    console.log(`spotify-json-2023.06.13`);
+let url = $request.url;
+// console.log(`原始url:${url}`);
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+    // console.log(`替换platform:${url}`);
+} else {
+    console.log('无需处理');
+}
+$done({
+    url
+});
+.log('无需处理');
+}
+$done({
+    url
+});
+uest.url;
+// console.log(`原始url:${url}`);
+if (url.inclconsole.log(`spotify-json-2023.06.13`);
+let url = $request.url;
+// console.log(`原始url:${url}`);
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+    // console.log(`替换platform:${url}`);
+} else {
+    console.log('无需处理');
+}
+$done({
+    url
+});
+udes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
     // console.log(`替换platform:${url}`);
 } else {
